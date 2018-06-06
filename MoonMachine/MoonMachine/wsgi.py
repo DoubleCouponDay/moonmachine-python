f79257fb5b1937fdc098c58ff40e37491df90e73
Sam <sjsuitandtie@outlook.com>
2018-06-06 20:28:32 +1000

    fixed fucking compilation error that didnt get picked up....

--- MoonMachine/MoonMachine/wsgi.py
+++ MoonMachine/MoonMachine/wsgi.py
@@ -18,9 +18,9 @@ from django.core.wsgi import get_wsgi_application
 import sys
 from django.core.management import execute_from_command_line
 
-root_path = os.path.abspath(os.path.split(__file__)[0])
-sys.path.insert(0, os.path.join(root_path, 'MoonMachine'))
-sys.path.insert(0, root_path) #fixed bug where wsgi boot didnt have path configured correctly
+BASE_DIR = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(BASE_DIR, 'MoonMachine'))
+sys.path.insert(0, BASE_DIR) #fixed bug where wsgi boot didnt have path configured correctly
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
