025653093a80f2957876baf849131a5db30ea470
Sam <sjsuitandtie@outlook.com>
2018-06-06 18:36:42 +1000

    moved settings to Back app

--- MoonMachine/MoonMachine/manage.py
+++ MoonMachine/MoonMachine/manage.py
@@ -17,7 +17,7 @@ try:
     if __name__ == "__main__":
         os.environ.setdefault(
             "DJANGO_SETTINGS_MODULE",
-            "settings"
+            "Back.settings"
         )
 
         from django.core.management import execute_from_command_line
