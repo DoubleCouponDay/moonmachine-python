4cf94dc75f9feec6add85ae3015aadabf3a9c7f6
Sam <sjsuitandtie@outlook.com>
2018-06-06 20:24:19 +1000

    wsgi didnt add static content. oops

--- MoonMachine/MoonMachine/wsgi.py
+++ MoonMachine/MoonMachine/wsgi.py
@@ -16,12 +16,18 @@ framework.
 import os
 from django.core.wsgi import get_wsgi_application
 import sys
+from django.core.management import execute_from_command_line
 
 root_path = os.path.abspath(os.path.split(__file__)[0])
 sys.path.insert(0, os.path.join(root_path, 'MoonMachine'))
-sys.path.insert(0, root_path)
+sys.path.insert(0, root_path) #fixed bug where wsgi boot didnt have path configured correctly
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+execute_from_command_line([os.path.join(BASE_DIR, "manage.py"), "check"])
+execute_from_command_line([os.path.join(BASE_DIR, "manage.py"), "makemigrations"])
+execute_from_command_line([os.path.join(BASE_DIR, "manage.py"), "migrate"])
+execute_from_command_line([os.path.join(BASE_DIR, "manage.py"), "collectstatic", "--noinput"])
 
 
 # This application object is used by any WSGI server configured to use this
