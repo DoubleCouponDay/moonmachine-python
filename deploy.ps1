684833a6bc7f4ec2a0bd719ab4fe69b89a4ae485
Sam <sjsuitandtie@outlook.com>
2018-06-13 15:34:09 +1000

    ...

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,6 @@
-﻿heroku ps:scale web=1 --app moonmachine2
-git push moonmachine2 master
-Read-Prompt -Prompt 'press enter when ready to move app to production'
-heroku pipelines:promote --app moonmachine2 --to moonmachine
-heroku ps:scale web=0 --app moonmachine2
+﻿heroku ps:scale web=1 --app moonmachine-staging
+git push moonmachine-staging master
+Read-Host -Prompt 'press enter when ready to move app to production'
+heroku pipelines:promote --app moonmachine-staging --to moonmachine
+heroku ps:scale web=0 --app moonmachine-staging
+Read-Host -Prompt 'script completed'
