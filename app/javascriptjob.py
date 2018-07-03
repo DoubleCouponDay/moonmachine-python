ae3897a53300ee643701e9dc25182f8222fddb3b
Sam <sjsuitandtie@outlook.com>
2018-07-03 10:02:55 +1000

    ..

--- app/javascriptjob.py
+++ app/javascriptjob.py
@@ -1,16 +1,23 @@
 import os
 from settings import DEBUG
+from logging import getLogger
 
 def javascriptjob():
     """using grunt because there will be multiple bundles in the future and separate node_modules files.
-    """
-    os.chdir(os.path.abspath("../node_modules/.bin"))
+    """    
+    logger = getLogger("javascriptjob")
 
     if DEBUG:         
-        
+        os.chdir(os.path.abspath("../node_modules/.bin"))    
         os.system("grunt development" + " --gruntfile ../../grunt_webjob.js --verbose")
+        os.chdir(os.path.abspath("../../app"))  
 
     else:
+        gruntpath = os.path.abspath("../node_modules/.bin")
+        logger.info("attempting gruntpath: " + gruntpath)
+        os.chdir(gruntpath)
         os.system("grunt production" + " --gruntfile ../../grunt_webjob.js --verbose")
-
-    os.chdir(os.path.abspath("../../app"))
+        returnpath = "../../app"
+        logger.info("attempting returnpath: " + returnpath)
+        os.chdir(returnpath)
+    
