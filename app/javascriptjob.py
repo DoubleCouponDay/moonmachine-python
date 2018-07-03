761a98f963eaa765ee5ea5f97023cc7e37fc4719
Sam <sjsuitandtie@outlook.com>
2018-07-04 09:23:42 +1000

    testing grunt files in their own directory
    this allows me to add them to my vsc workspace and use linting.

--- app/javascriptjob.py
+++ app/javascriptjob.py
@@ -6,18 +6,19 @@ def javascriptjob():
     """using grunt because there will be multiple bundles in the future and separate node_modules files.
     """    
     logger = getLogger("javascriptjob")
+    commonparams = " --gruntfile ../../grunt/grunt_webjob.js --verbose"
 
     if DEBUG:         
         os.chdir(os.path.abspath("../node_modules/.bin"))    
-        os.system("grunt development" + " --gruntfile ../../grunt_webjob.js --verbose")
+        os.system("grunt development" + commonparams)
         os.chdir(os.path.abspath("../../app"))  
 
     else:
         gruntpath = os.path.abspath("./node_modules/.bin")
-        logger.info("attempting gruntpath: " + gruntpath)
+        logger.info("attempting grunt dir: " + gruntpath)
         os.chdir(gruntpath)
-        os.system("grunt production" + " --gruntfile ../../grunt_webjob.js --verbose")
+        os.system("grunt production" + commonparams)
         returnpath = "../../app"
-        logger.info("attempting returnpath: " + returnpath)
+        logger.info("attempting return dir: " + returnpath)
         os.chdir(returnpath)
     
---
+++ grunt/grunt_webjob.js
@@ -0,0 +1,230 @@
+let parcel = require("parcel");
+let async = require("async");
+let path = require("path");
+let filesystem = require("fs");
+
+//pipeline
+const INPUTFOLDERREL = "../app/front/";
+const OUTPUTFOLDERREL = "../app/static_pipeline/";
+const OUTPUTFOLDERABS = path.join(__dirname, OUTPUTFOLDERREL);
+const OUTPUTJAVASCRIPTFOLDER = OUTPUTFOLDERABS + "scripts/";
+
+//parcel
+const BINDERSFOLDEREL = "custom/binders";
+const INPUTBUNDLEFOLDER = OUTPUTJAVASCRIPTFOLDER + BINDERSFOLDEREL;
+
+const jsextension = ".js";
+
+module.exports = function(grunt) //using the nodejs module format
+{	
+	let configobject = {
+		pkg: grunt.file.readJSON('../package.json'),
+
+		browserify: { //incorporating browserify breaking changes ugh
+			development: {
+				files: [],
+
+				options: {
+					debug: true
+				}
+			},
+			production: {
+				files: [],
+				
+				options: {
+				
+				}
+			}
+		},
+		
+		babel: {
+			production: {
+				files: []
+			}
+		}
+	};
+	configobject = SetPaths(configobject);
+	grunt.initConfig(configobject);		
+	
+	require('load-grunt-tasks')(grunt);
+	
+	grunt.registerTask("pipeline", "description", pipelinetask);
+	grunt.registerTask("parcel", "description", parceltask);
+	
+	grunt.registerTask("development", ["pipeline", "parcel:true"]);
+	grunt.registerTask("production", ["pipeline", "babel", "parcel:false"]);
+};
+
+function SetPaths(configobject)
+{
+	let alljsfiles = { //dynamic file mappings
+		expand: true,
+		cwd: OUTPUTJAVASCRIPTFOLDER,
+		src: ["**/*.js"],
+		dest: OUTPUTJAVASCRIPTFOLDER	
+	};
+	configobject.babel.production.files = [alljsfiles];
+	return configobject;
+}
+
+function pipelinetask()
+{
+	deleteoldpipeline();
+	collectfronttopipeline();
+}
+
+function parceltask(debugmodebool) 
+{	
+	let done = this.async(); //grunt specific command for making an asynchronous task.	
+	renamebundlestomin();	
+	buildbundles(debugmodebool, done);
+}
+
+function deleteoldpipeline()
+{
+	console.log("___________________________________")
+	console.log("beginning " + deleteoldpipeline.name);
+
+	foldertraverser(OUTPUTFOLDERREL, function onfile (currentdirectory, currentfile)
+	{
+		let itemsfullpath = path.join(__dirname, currentdirectory, currentfile);
+		filesystem.unlinkSync(itemsfullpath);
+		console.log(itemsfullpath + " deleted.");
+	},
+	null);
+}
+
+function collectfronttopipeline()
+{
+	console.log("___________________________________")
+	console.log("beginning " + collectfronttopipeline.name);
+
+	foldertraverser(INPUTFOLDERREL, function onfile (currentdirectory, currentfile, addeddirectories) 
+	{
+		let itemsfullpath = path.join(__dirname, currentdirectory, currentfile);	
+		let itemsoutputpath = path.join(OUTPUTFOLDERABS, addeddirectories, currentfile);
+		console.log("current path: " + itemsfullpath);
+		console.log("destination path: " + itemsoutputpath);			
+		console.log("copying...");
+		filesystem.copyFileSync(itemsfullpath, itemsoutputpath);		
+	},
+	function ondirectory(currentdirectory, currentfile, addeddirectories)
+	{
+		let itemsoutputdir = path.join(OUTPUTFOLDERABS, addeddirectories, currentfile);	
+		
+		if (filesystem.existsSync(itemsoutputdir) === false)
+		{
+			console.log("creating mirror directory: " + itemsoutputdir);
+			filesystem.mkdirSync(itemsoutputdir);			
+		}
+	});
+}
+
+//callbacks are given currentdirectory, currentfilename, addeddirectories.
+//currentdirectory must be relative to the location of this grunt file.
+//onnewdirectory can be null.
+function foldertraverser(relativedirectory, onfile, onnewdirectory)
+{
+	const standardpreprend = "/";
+	const addedindex = 3;
+	const isnotgiven = () => arguments[addedindex] !== undefined && typeof (arguments[addedindex]) === "string";
+	let addeddirectoriesstr = isnotgiven() ? arguments[addedindex] : ""; //access an overload argument. hopefully not given by the invoker.
+	console.log("NEW TRAVERSAL");
+	let files = filesystem.readdirSync(relativedirectory);
+
+	for (let i = 0; i < files.length; i++)
+	{
+		let itemsrelativepath = path.join(relativedirectory, files[i]);
+		let itemsstatus = filesystem.lstatSync(itemsrelativepath);			
+
+		if(itemsstatus.isFile() === true)
+		{
+			console.log("current item '" + files[i] + "' is file.");			
+			onfile(relativedirectory, files[i], addeddirectoriesstr);
+		}
+
+		else if(itemsstatus.isDirectory() === true)
+		{
+			console.log("current item '" + files[i] + "' is directory.");
+
+			if (onnewdirectory !== null)
+			{
+				onnewdirectory(relativedirectory, files[i], addeddirectoriesstr);
+			}			
+			let nextlayeradditions = addeddirectoriesstr + standardpreprend + files[i]; //take a copy since this addition is only relevant to the next layer.			
+			foldertraverser(itemsrelativepath, onfile, onnewdirectory, nextlayeradditions);
+		}
+	}
+}
+
+function renamebundlestomin()
+{
+	console.log("renaming bundle inputs so that source maps may work.");
+	let files = filesystem.readdirSync(INPUTBUNDLEFOLDER);
+
+	for (let i = 0; i < files.length; i++)
+	{
+		console.log("renaming " + files[i]);
+		let filenamecomponents = files[i].split(".");
+		let newpath = INPUTBUNDLEFOLDER + "/" + filenamecomponents[0] + ".build" + jsextension;
+		let oldpath = path.join(INPUTBUNDLEFOLDER, files[i]);
+		filesystem.renameSync(oldpath, newpath);
+	}
+}
+
+function buildbundles(debugmodebool, finishedtask)
+{	
+	let parceloptions = {
+		outDir: INPUTBUNDLEFOLDER,
+		cache: false,
+		logLevel: 3,
+		minify: debugmodebool === false,
+		sourceMaps: debugmodebool,
+		watch: false
+	};
+	console.log("parcel minify: " + parceloptions["minify"]);
+	console.log("parcel sourcemaps: " + parceloptions["sourceMaps"]);
+
+	let possibleherokuport = process.env.PORT;
+	let hmrportkey = "hmrPort";
+
+	if (possibleherokuport != null)
+	{
+		console.log("heroku port found. setting parcel port to " + possibleherokuport);
+		parceloptions[hmrportkey] = possibleherokuport;
+	}
+
+	let files = filesystem.readdirSync(INPUTBUNDLEFOLDER);
+	let outfilekey = "outFile";
+	console.log("found " + files.length + " entrypoints to bundle.");
+
+	async.eachSeries(files, (file, continueloop) => {
+		console.log("processing " + file + " into a bundle.");
+		let buildfilecomponents = file.split(".");		
+		parceloptions[outfilekey] = buildfilecomponents[0] + jsextension;
+		let absolutelocation = path.join(INPUTBUNDLEFOLDER, file);
+		let bundler = new parcel(absolutelocation, parceloptions);
+		let newpath = path.join(INPUTBUNDLEFOLDER, parceloptions[outfilekey]);
+		
+		bundler.bundle()
+		.then(() => {					
+			console.log("waiting for bundle to exist...");
+
+			let fileexists = false;
+
+			while(fileexists == false)
+			{
+				fileexists = filesystem.existsSync(newpath);
+
+				if (fileexists)
+				{
+					console.log("bundle exists.");
+					continueloop();
+				}
+			}
+		});
+	},
+	function onfinished(){
+		finishedtask();
+	});			
+}
