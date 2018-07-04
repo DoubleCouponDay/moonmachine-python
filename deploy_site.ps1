1484b115a8636f927145492edb646851c79a6630
Sam <sjsuitandtie@outlook.com>
2018-07-04 12:18:55 +1000

    ...

--- deploy_site.ps1
+++ deploy_site.ps1
@@ -3,7 +3,6 @@ heroku ps:scale web=1 --app moonmachine-staging
 git push moonmachine-staging master
 $promotionverdict = Read-Host -Prompt 'promote to production? [y/n]'
 
-
 if ($promotionverdict -eq 'y')
 {    
     heroku pipelines:promote --app moonmachine-staging --to moonmachine    
