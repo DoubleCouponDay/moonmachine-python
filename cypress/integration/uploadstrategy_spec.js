1fc5bc81e699a1f5401f711434d42e7bb1452411
Sam <sjsuitandtie@outlook.com>
2018-07-04 19:48:29 +1000

    working on this new sexy integration testing framework

--- cypress/integration/uploadstrategy_spec.js
+++ cypress/integration/uploadstrategy_spec.js
@@ -1,6 +1,27 @@
-describe("uploadstrategy", function() {
-    it("returnshttp200", function() {
+describe("uploadstrategy", () => {
+    it("werks", () => {
         cy.visit("/admin/porfolio")
-            .
+            .wait(3000)
+            .then(() => {
+                cy.get("#authfilebox")
+                    .click()
+            })
+            .then(() => {
+                cy.get("button")
+                    .contains("Upload")
+                    .click()
+            })
+            .then(() => {
+                cy.get("#dooting")
+                    .should("")
+            })
+            .then(() => {
+                cy.server()
+                .wait()
+            })
+            .then(() => {
+                cy.get("#dooting")
+                    .should("not.be.visible");
+            })
     });
 });
